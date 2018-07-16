706d8a2d3a93969db6580b28e36df2e50fac73ee
titlethanason <titlethanason@gmail.com>
2018-07-16 15:39:43 +0700

    first to commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var mongojs = require('mongojs');
+var db = mongojs('iottest');
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+var HOST = '0.0.0.0';
+var PORT = 80;
+
+server.on("listening",function(){
+    var address = server.address();
+    console.log("Listening UDP on "+address.addres + " : "+ address.port);
+});
+
+server.on("message",function(message,remote){
+    console.log(remote.address + " : " + remote.port + " - " +message);
+    var strMessage = JSON.parse(message);
+    console.log("strMessage = "+strMessage);
+
+    // var db_c = db.collection('iottest1');
+    // var d = new Date();
+    // var utc = d.getTime();
+})
+
+server.bind(PORT);

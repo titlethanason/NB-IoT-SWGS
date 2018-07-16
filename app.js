56a1ee820bc361fad66b58165deffa7a26ed0bb2
titlethanason <titlethanason@gmail.com>
2018-07-16 16:58:22 +0700

    update server.on error

--- app.js
+++ app.js
@@ -3,22 +3,23 @@ var db = mongojs('iottest');
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
-var HOST = '0.0.0.0';
 var PORT = 4000;
 
 server.on("listening",function(){
     var address = server.address();
-    console.log("Listening UDP on "+address.address + " : "+ address.port);
+    console.log("Listening UDP on "+address.address + ":"+ address.port);
 });
 
 server.on("message",function(message,remote){
+    console.log("Recieve request !!");
     console.log(remote.address + " : " + remote.port + " - " +message);
     var strMessage = JSON.parse(message);
     console.log("strMessage = "+strMessage);
+});
 
-    // var db_c = db.collection('iottest1');
-    // var d = new Date();
-    // var utc = d.getTime();
-})
+server.on("error",function(err){
+    console.log("server error : "+err);
+    server.close();
+});
 
 server.bind(PORT);
